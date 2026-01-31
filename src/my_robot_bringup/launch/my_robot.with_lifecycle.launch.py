e9b5629126afc3006795943304788cd7996e07e0
SphericalCowww <tinglin194@gmail.com>
2026-01-31 17:41:25 +0100

    - updating the commander from a jumbo code to a lifecycle

---
+++ src/my_robot_bringup/launch/my_robot.with_lifecycle.launch.py
@@ -0,0 +1,93 @@
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+from launch_ros.actions import Node, SetParameter
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from moveit_configs_utils import MoveItConfigsBuilder
+import os
+
+######################################################################################################################
+def generate_launch_description():
+    robot_description_path   = get_package_share_path('my_robot_description')
+    robot_bringup_path       = get_package_share_path('my_robot_bringup')
+    robot_moveit_config_path = get_package_share_path('my_robot_moveit_config')   
+ 
+    urdf_path          = os.path.join(robot_description_path,   'urdf',   'my_robot.urdf.xacro')
+    robot_controllers  = os.path.join(robot_bringup_path,       'config', 'my_robot_controllers.yaml')
+    moveit_config_path = os.path.join(robot_moveit_config_path, 'launch', 'move_group.launch.py')
+    rviz_config_path   = os.path.join(robot_description_path,   'rviz',   'my_robot.urdf_config.rviz')
+
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': robot_description}],
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            robot_controllers,
+            {'use_steady_clock_for_update': False},],
+    )
+    # check src/my_robot_bringup/config/my_robot_controllers.yaml 
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+    leg1_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["leg1_controller"],
+    )
+    
+    moveit_config = (
+        MoveItConfigsBuilder("my_robot", package_name="my_robot_moveit_config")
+        .robot_description(file_path=urdf_path)
+        .to_moveit_configs()
+    )
+    moveit_launcher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(moveit_config_path),
+        launch_arguments={}.items(),
+    )
+    lifecycle_node = Node(
+        package="my_robot_lifecycle",
+        executable="my_robot_lifecycle",
+        parameters=[
+            moveit_config.robot_description,           # the URDF math
+            moveit_config.robot_description_semantic,  # the SRDF (defines 'leg1')
+            moveit_config.robot_description_kinematics,# the kinematics.yaml
+            moveit_config.joint_limits,                # the joint_limits.yaml 
+            {"jump_threshold": 0.15},                  # for computeCartesianPath
+        ],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_path],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
+    )
+    
+    return LaunchDescription([
+        SetParameter(name='jump_threshold', value=0.15),
+        SetParameter(name='use_sim_time', value=False),
+        robot_state_publisher_node,
+        control_node,
+        joint_state_broadcaster_spawner,
+        leg1_controller_spawner,
+        moveit_launcher,
+        lifecycle_node,
+        rviz_node,
+    ])
+
+######################################################################################################################
---
+++ src/my_robot_commander/src/my_robot_lifecycle.cpp
@@ -0,0 +1,171 @@
+#include <memory>
+#include <thread>
+#include <string>
+#include <thread>
+#include <cmath>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "lifecycle_msgs/msg/state.hpp"
+#include "moveit/move_group_interface/move_group_interface.hpp"
+#include <moveit/robot_state/robot_state.hpp>
+#include <moveit/robot_model/robot_model.hpp>
+#include <moveit/robot_trajectory/robot_trajectory.hpp>
+#include <moveit/trajectory_processing/time_optimal_trajectory_generation.hpp>
+#include <trajectory_msgs/msg/joint_trajectory_point.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+
+using rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface;
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
+using namespace std::placeholders;                  // for using _1
+ 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//https://github.com/ros2/example_interfaces/tree/rolling/msg
+#include <example_interfaces/msg/string.hpp>
+#include <example_interfaces/msg/float64_multi_array.hpp>    //variable size
+#include "my_robot_interface/msg/my_robot_leg1_pose_target.hpp"
+#include <example_interfaces/msg/bool.hpp>
+using ros_string   = example_interfaces::msg::String;
+using ros_array    = example_interfaces::msg::Float64MultiArray;
+using custom_array = my_robot_interface::msg::MyRobotLeg1PoseTarget;
+using ros_bool     = example_interfaces::msg::Bool;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class MyRobotLifecycleManager : public rclcpp_lifecycle::LifecycleNode {
+public:
+    explicit MyRobotLifecycleManager(const rclcpp::NodeOptions & options)
+    : rclcpp_lifecycle::LifecycleNode("my_robot_lifecycle", options) {
+        RCLCPP_INFO(get_logger(), "constructor(): %s", current_lifecycle_state_.c_str());
+
+        current_lifecycle_state_ = "state_initialized";
+        RCLCPP_INFO(get_logger(), "constructor(): %s", current_lifecycle_state_.c_str());
+    }
+
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &) override {
+        RCLCPP_INFO(get_logger(), "on_configure(): %s", current_lifecycle_state_.c_str());
+
+        callback_group_ = create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        leg1_interface_ = std::make_shared<MoveGroupInterface>(
+            this->get_node_base_interface(),
+            this->get_node_graph_interface(),
+            this->get_node_logging_interface(),
+            this->get_node_services_interface(),
+            planning_group_
+        );
+        leg1_interface_->setEndEffectorLink(endEffector_link_);
+        leg1_interface_->setPlanningTime(5.0);
+        if (!leg1_interface_->startStateMonitor(2.0)) {
+            RCLCPP_ERROR(get_logger(), "on_configure(): failed to start state monitor");
+            return CallbackReturn::FAILURE;
+        }
+        
+        leg1_load_current_robot_state_();
+        RCLCPP_INFO(get_logger(), "on_configure(): current end effector (x, y, z) = (%lf, %lf, %lf)",
+                    endEffector_x_, endEffector_y_, endEffector_z_);
+        current_lifecycle_state_ = "state_configured";
+        RCLCPP_INFO(get_logger(), "on_configure(): %s", current_lifecycle_state_.c_str());
+        return CallbackReturn::SUCCESS;
+    }
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &) override {
+        RCLCPP_INFO(get_logger(), "on_activation(): %s", current_lifecycle_state_.c_str());
+
+        auto sub_options = rclcpp::SubscriptionOptions();
+        sub_options.callback_group = callback_group_;
+        leg1_named_subscriber_ = create_subscription<example_interfaces::msg::String>("/leg1_set_named", 10,
+            std::bind(&MyRobotLifecycleManager::leg1NamedCallback, this, std::placeholders::_1), sub_options);
+
+        current_lifecycle_state_ = "state_stationary";
+        RCLCPP_INFO(get_logger(), "on_activation(): %s", current_lifecycle_state_.c_str());
+        return CallbackReturn::SUCCESS;
+    }
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override {
+        RCLCPP_INFO(get_logger(), "on_deactivation(): %s", current_lifecycle_state_.c_str());
+
+        leg1_named_subscriber_.reset();
+        leg1_interface_->stop();
+
+        current_lifecycle_state_ = "state_stopped";
+        RCLCPP_INFO(get_logger(), "on_deactivation(): %s", current_lifecycle_state_.c_str());
+        return CallbackReturn::SUCCESS;
+    }
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &) override {
+        RCLCPP_INFO(get_logger(), "on_cleanup(): %s", current_lifecycle_state_.c_str());
+
+        leg1_interface_.reset();
+
+        current_lifecycle_state_ = "state_configured";
+        RCLCPP_INFO(get_logger(), "on_clean(): %s", current_lifecycle_state_.c_str());
+        return CallbackReturn::SUCCESS;
+    }
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &) override {
+        RCLCPP_INFO(get_logger(), "on_shutdown(): %s", current_lifecycle_state_.c_str());
+
+        leg1_interface_->stop();
+
+        current_lifecycle_state_ = "state_stopped";
+        RCLCPP_INFO(get_logger(), "on_shutdown(): %s", current_lifecycle_state_.c_str());
+        return CallbackReturn::SUCCESS;
+    }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+private:
+    void leg1NamedCallback(const example_interfaces::msg::String::SharedPtr msg) {
+        leg1_load_current_robot_state_();
+        RCLCPP_INFO(get_logger(), "leg1NamedCallback(): current end effector (x, y, z) = (%lf, %lf, %lf)",
+                    endEffector_x_, endEffector_y_, endEffector_z_);
+        leg1_interface_->setNamedTarget(msg->data);
+        planAndExecute();
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void planAndExecute() {
+        MoveGroupInterface::Plan plan;
+        success_ = (leg1_interface_->plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
+        if (success_) {
+            leg1_interface_->execute(plan);
+        } else {
+            RCLCPP_WARN(get_logger(), "planAndExecute(): planning failed");
+        }
+    }
+    void leg1_load_current_robot_state_() {
+        current_robot_state_ = leg1_interface_->getCurrentState(0.5);     // wait up to 0.5 seconds 
+        if (!current_robot_state_) {
+            RCLCPP_ERROR(get_logger(), "leg1_load_current_robot_state_(): timeout to fetch current robot state");
+        }
+        endEffector_pose_ = leg1_interface_->getCurrentPose(endEffector_link_);
+        endEffector_x_ = endEffector_pose_.pose.position.x;
+        endEffector_y_ = endEffector_pose_.pose.position.y;
+        endEffector_z_ = endEffector_pose_.pose.position.z;
+        leg1_interface_->setStartStateToCurrentState();
+        //leg1_interface_->setStartState(*leg1_interface_->getCurrentState()); // faster, but risk stalling
+    }
+
+    std::shared_ptr<MoveGroupInterface> leg1_interface_;
+    rclcpp::CallbackGroup::SharedPtr callback_group_;
+    rclcpp::Subscription<example_interfaces::msg::String>::SharedPtr leg1_named_subscriber_;
+
+    std::atomic<bool> success_{false};
+    std::string current_lifecycle_state_    = "state_uninitialized";
+    std::string planning_group_   = "leg1";
+    std::string endEffector_link_ = "calfSphere";
+    moveit::core::RobotStatePtr     current_robot_state_;
+    geometry_msgs::msg::PoseStamped endEffector_pose_;
+    double endEffector_x_ = 0;
+    double endEffector_y_ = 0;
+    double endEffector_z_ = 0;
+    double to_target_dist       = 0;
+    double to_target_dist_thres = 0.01;
+};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions node_options;
+    node_options.automatically_declare_parameters_from_overrides(true);
+    auto node = std::make_shared<MyRobotLifecycleManager>(node_options);
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node->get_node_base_interface());
+    executor.spin();
+    rclcpp::shutdown();
+    return 0;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
